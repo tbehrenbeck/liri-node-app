319f4e8815969b1c2e0608a9b63c5d0b3bdf2b5a
Tommie Behrenbeck <behrenbt@gmail.com>
2018-08-19 16:06:37 -0500

    omdb

--- keys.js
+++ keys.js
@@ -1,8 +1,16 @@
 console.log('this is loaded');
 
-exports.spotify = {
+var spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-//var spotify = new Spotify(keys.spotify);
+var omdb = process.env.OMDB_KEY;
+
+var bandsintown = process.env.BANDS_IN_TOWN_KEY;
+
+module.exports = {
+  spotify: spotify,
+  omdb: omdb,
+  bandsintown: bandsintown
+}
--- liri.js
+++ liri.js
@@ -1,14 +1,29 @@
 require("dotenv").config();
-
+var request = require("request");
+var keys = require("./keys");
+var movie; 
 //var spotify = new Spotify(keys.spotify);
+
+// switch statements 
+var input= process.argv[2]
+switch(input) {
+  case "movie-this":
+  getMovie();
+      break;
+  // case y:
+  //     code block
+  //     break;
+  // default:
+  //     code block
+}
 
 
 //------OMDB--------
-var request = require("request");
-var movie= process.argv[2];
+function getMovie () {
+  movie= process.argv[3];
 
-request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + keys.omdb;
+  request (queryUrl, function(error, response, body) {
   if (!error && response.statusCode === 200) {
 
     console.log("==============================================");
@@ -16,14 +31,16 @@ request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", 
     console.log("Movie title: " + JSON.parse(body).Title);
     console.log("Release year: " + JSON.parse(body).Year);
     console.log("IMDB rating: " + JSON.parse(body).imdbRating);
-    console.log(JSON.parse(body).Ratings[1]);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
     console.log("Movie production location: " + JSON.parse(body).Country);
     console.log("Languages: " + JSON.parse(body).Language);
     console.log("Plot: " + JSON.parse(body).Plot);
     console.log("Actors: " + JSON.parse(body).Actors);
     console.log("");
     console.log("==============================================");
+
   } else {
       console.log(error);
   }
 });
+};
