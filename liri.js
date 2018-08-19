e59dc2049ec202acdc31a2245b2ca6ef4ea16004
Tommie Behrenbeck <behrenbt@gmail.com>
2018-08-19 17:39:41 -0500

    bandsInTown

--- liri.js
+++ liri.js
@@ -1,22 +1,23 @@
 require("dotenv").config();
 var request = require("request");
 var keys = require("./keys");
-var movie; 
-//var spotify = new Spotify(keys.spotify);
+//var spotify = require('node-spotify-api');
+//var moment = require("moment");
+var movie;
+var artist;
 
 // switch statements 
 var input= process.argv[2]
 switch(input) {
   case "movie-this":
-  getMovie();
+      getMovie();
       break;
-  // case y:
-  //     code block
-  //     break;
+  case "concert-this":
+      getBands();
+      break;
   // default:
   //     code block
 }
-
 
 //------OMDB--------
 function getMovie () {
@@ -24,23 +25,52 @@ function getMovie () {
 
   var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + keys.omdb;
   request (queryUrl, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+    if (!error && response.statusCode === 200) {
 
-    console.log("==============================================");
-    console.log("");
-    console.log("Movie title: " + JSON.parse(body).Title);
-    console.log("Release year: " + JSON.parse(body).Year);
-    console.log("IMDB rating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-    console.log("Movie production location: " + JSON.parse(body).Country);
-    console.log("Languages: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-    console.log("");
-    console.log("==============================================");
+      console.log("==============================================");
+      console.log("");
+      console.log("Movie title: " + JSON.parse(body).Title);
+      console.log("Release year: " + JSON.parse(body).Year);
+      console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Movie production location: " + JSON.parse(body).Country);
+      console.log("Languages: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("");
+      console.log("==============================================");
 
-  } else {
-      console.log(error);
-  }
-});
+    } else {
+        console.log(error);
+    }
+  });
 };
+
+//-----Bands in Town------
+function getBands () {
+  artist= process.argv[3];
+
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown;
+  request (queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("==============================================");
+      console.log("");
+      console.log("Venue: " + JSON.parse(body)[0].venue.name);
+      console.log("Location: " + JSON.parse(body)[0].venue.city + "," + JSON.parse(body)[0].venue.country);
+      console.log(JSON.parse(body)[0].datetime);
+      // console.log(moment((body)[0].datetime, 'YYYY-MM-DD').format('MM/DD/YYYY'));
+      console.log("");
+      console.log("==============================================");
+    } else {
+      console.log(error);
+    }
+  });
+};
+
+//-----Spotify---- 
+
+// function getSongs () {
+//   song= process.argv[3];
+  
+//   var queryUrl = "https://api.spotify.com/v1/search?q=lucky&type=track"
+// }
