c781588bd6c433c13c99f02a22cb7985488f59a4
Tommie Behrenbeck <behrenbt@gmail.com>
2018-08-20 10:01:55 -0500

    spotify

--- liri.js
+++ liri.js
@@ -2,22 +2,26 @@ require("dotenv").config();
 var request = require("request");
 var keys = require("./keys");
 const chalk = require('chalk');
-//var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 //var moment = require("moment");
 var movie;
 var artist;
+var song;
 
 // switch statements 
 var input= process.argv[2]
 switch(input) {
-  case "movie-this":
+  case "movie-this": // node liri.js movie-this <movie name>
       getMovie();
       break;
-  case "concert-this":
+  case "concert-this": // node liri.js concert-this <artist name> 
       getBands();
       break;
+  case "spotify-this-song": // node liri.js spotify-this-song <song name> 
+      getSong();
+      break;
   // default:
-  //     code block
+  // code block
 }
 
 //------OMDB--------
@@ -29,7 +33,6 @@ function getMovie () {
     if (!error && response.statusCode === 200) {
 
       console.log("==============================================");
-      console.log("");
       console.log("\n" + chalk.redBright.underline("Title") + ": " + JSON.parse(body).Title + "\n");
       console.log(chalk.redBright("Release year: ") + JSON.parse(body).Year);
       console.log(chalk.redBright("IMDB Rating: ") + JSON.parse(body).imdbRating);
@@ -38,7 +41,6 @@ function getMovie () {
       console.log(chalk.redBright("Languages: ") + JSON.parse(body).Language);
       console.log(chalk.redBright("Actors: ") + JSON.parse(body).Actors);
       console.log("\n" + chalk.redBright("Plot: ") + JSON.parse(body).Plot + "\n");
-      console.log("");
       console.log("==============================================");
 
     } else {
@@ -55,12 +57,11 @@ function getBands () {
   request (queryUrl, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       console.log("==============================================");
-      console.log("");
-      console.log("\n" + chalk.blueBright.underline("Venue") + ": " + JSON.parse(body)[0].venue.name + "\n");
+      console.log("\n" + chalk.blueBright.underline("Artist") + ": " + artist + "\n");
+      console.log(chalk.blueBright("Venue") + ": " + JSON.parse(body)[0].venue.name);
       console.log(chalk.blueBright("Location") + ": " + JSON.parse(body)[0].venue.city + "," + JSON.parse(body)[0].venue.country);
       console.log(chalk.blueBright("Time") + ": " + JSON.parse(body)[0].datetime);
-      // console.log(moment((body)[0].datetime, 'YYYY-MM-DD').format('MM/DD/YYYY'));
-      console.log("");
+      // console.log(moment((body)[0].datetime).format('MM/DD/YYYY'));
       console.log("==============================================");
     } else {
       console.log(error);
@@ -69,9 +70,19 @@ function getBands () {
 };
 
 //-----Spotify---- 
+function getSong() {
+  song= process.argv[3];
 
-// function getSongs () {
-//   song= process.argv[3];
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
   
-//   var queryUrl = "https://api.spotify.com/v1/search?q=lucky&type=track"
-// }
+  spotify.search({ type: 'track', query: song}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  
+  console.log(JSON.parse(data)); 
+  });
+}
